bdfcc7da8cf8247bb9049bafd7deced5cf7a9d6e
AdamSokolowski <a.sokolowski84@gmail.com>
2018-04-28 16:29:00 +0200

    Warsztaty3 drukowanie w index.jsp zad2

---
+++ WebContent/views/index.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<table border="1">
+	  <thead>
+	  	<tr>
+	  	  <th scope="col">Created</th>
+	  	  <th scope="col">Updated</th>
+	  	  <th scope="col">Description</th>
+	  	  <th scope="col">ExcerciseId</th>
+	  	  <th scope="col">userId</th>
+	  	</tr>
+	  </thead>
+	  <tbody>
+	  <c:forEach var="solution" items="${solutions}">
+	  	<tr>
+	  		<td>${solution.getCreated()}</td>
+	  		<td>${solution.getUpdated()}</td>
+	  		<td>${solution.getDescription()}</td>
+	  		<td>${solution.getExcerciseId()}</td>
+	  		<td>${solution.userId()}</td>
+	  	</tr>
+	  </c:forEach>
+	  </tbody>	  
+	</table>
+	
+</body>
+</html>
---
+++ src/pl/coderslab/controller/ServletStronyStartowej.java
@@ -0,0 +1,55 @@
+package pl.coderslab.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.coderslab.model.Solution;
+
+/**
+ * Servlet implementation class ServletStronyStartowej
+ */
+@WebServlet("/")
+public class ServletStronyStartowej extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletStronyStartowej() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Solution solution = new Solution();
+		
+		int numberSolutions = Integer.parseInt(getServletContext().getInitParameter("number-solutions"));
+		try {
+			Solution[] solutions = solution.loadAll(numberSolutions);
+			request.setAttribute("solutions", solutions);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		getServletContext().getRequestDispatcher("/views/index.jsp").forward(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
